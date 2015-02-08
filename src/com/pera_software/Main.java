df90dcaf583ce4d6a315243553b84248595c2e21
P. Most <PMost@PERA-Software.com>
2015-02-08 22:12:04 +0100

    First steps for tree

--- src/com/pera_software/Main.java
+++ src/com/pera_software/Main.java
@@ -1,7 +1,35 @@
 package com.pera_software;
 
-public class Main {
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
-	public static void main( String arguments ) {
+public class Main extends Application {
+
+	public static void main( String arguments[] ) {
+		launch( arguments );
+	}
+
+	@Override
+	public void start( Stage stage ) throws Exception {
+		TreeItem< String > root = new TreeItem<>( "CMWMarsAndLoggingServer" );
+		root.setExpanded( true );
+		
+		TreeItem< String > loggingServerItem = new TreeItem<>( "LoggingServer" );
+		loggingServerItem.getChildren().add( new TreeItem<>( "Boost" ));
+		
+		root.getChildren().add( loggingServerItem );
+		root.getChildren().add( new TreeItem<>( "LoggingServerCUI" ));
+		
+		TreeView< String > tree = new TreeView<>( root );
+		
+		StackPane pane = new StackPane();
+		pane.getChildren().add( tree );
+		
+		Scene scene = new Scene( pane );
+		stage.setScene( scene );
+		stage.show();
 	}
 }

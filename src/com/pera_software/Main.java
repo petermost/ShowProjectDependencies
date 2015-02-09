32f6206054fc266de501309b7adc6ad930c9c5c7
P. Most <PMost@PERA-Software.com>
2015-02-09 18:14:10 +0100

    Added arguments parsing

--- src/com/pera_software/Main.java
+++ src/com/pera_software/Main.java
@@ -1,35 +1,45 @@
 package com.pera_software;
 
+import java.nio.file.*;
 import javafx.application.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
+import com.pera_software.aidkit.visualstudio.*;
 
 public class Main extends Application {
 
 	public static void main( String arguments[] ) {
+		Settings.parseCommandLine( arguments );
 		launch( arguments );
 	}
 
 	@Override
 	public void start( Stage stage ) throws Exception {
-		TreeItem< String > root = new TreeItem<>( "CMWMarsAndLoggingServer" );
-		root.setExpanded( true );
-		
-		TreeItem< String > loggingServerItem = new TreeItem<>( "LoggingServer" );
-		loggingServerItem.getChildren().add( new TreeItem<>( "Boost" ));
-		
-		root.getChildren().add( loggingServerItem );
-		root.getChildren().add( new TreeItem<>( "LoggingServerCUI" ));
-		
-		TreeView< String > tree = new TreeView<>( root );
-		
-		StackPane pane = new StackPane();
-		pane.getChildren().add( tree );
-		
-		Scene scene = new Scene( pane );
+		StackPane stackPane = new StackPane();
+
+		for ( String solutionFileName : Settings.solutionFileNames() ) {
+			Path solutionFilePath = Paths.get( solutionFileName );
+			SolutionFile solutionFile = new SolutionFile( solutionFilePath );
+			TreeItem< String > solutionItem = new TreeItem<>( solutionFilePath.getFileName().toString() );
+			for ( ProjectFile projectFile : solutionFile.findProjects() ) {
+				buildProjectTree( solutionItem, projectFile );
+			}
+			solutionItem.setExpanded( true );
+			TreeView< String > treeView = new TreeView<>( solutionItem );
+			stackPane.getChildren().add( treeView );
+		}
+		Scene scene = new Scene( stackPane );
 		stage.setScene( scene );
 		stage.show();
+	}
+
+	private static void buildProjectTree( TreeItem< String > parentItem , ProjectFile projectFile  ) throws Exception {
+		TreeItem< String > projectItem = new TreeItem<>( projectFile.path().getFileName().toString() );
+		parentItem.getChildren().add( projectItem );
+
+		for ( ProjectFile projectReference : projectFile.getProjectReferences( ) )
+			buildProjectTree( projectItem, projectReference );
 	}
 }

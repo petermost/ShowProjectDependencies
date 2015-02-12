669224e01c8150429bb7c0f7eb875435f1e000b3
P. Most <PMost@PERA-Software.com>
2015-02-12 14:20:12 +0100

    Build project tree in the background

--- src/com/pera_software/Main.java
+++ src/com/pera_software/Main.java
@@ -2,6 +2,7 @@ package com.pera_software;
 
 import java.nio.file.*;
 import java.util.*;
+import org.controlsfx.control.*;
 import javafx.application.*;
 import javafx.event.*;
 import javafx.scene.*;
@@ -20,20 +21,36 @@ public class Main extends Application {
 	private static final String CS_ICON_NAME = "resources/icons16x16/text-x-csharp.png";
 	private static final String LIB_ICON_NAME = "resources/icons16x16/shared-lib.png";
 
-	private static final Map< String, ImageView > ICONS = new HashMap<>();
+	private static final Image SLN_IMAGE = new Image( Main.class.getResourceAsStream( SLN_ICON_NAME ));
+	private static final Image CPP_IMAGE = new Image( Main.class.getResourceAsStream( CPP_ICON_NAME ));
+	private static final Image CS_IMAGE  = new Image( Main.class.getResourceAsStream( CS_ICON_NAME ));
+	private static final Image LIB_IMAGE = new Image( Main.class.getResourceAsStream( LIB_ICON_NAME ));
+
+	private static final Map< String, Image > ICONS = new HashMap<>();
 	static {
-		ICONS.put( SLN_ICON_NAME, new ImageView( SLN_ICON_NAME ));
-		ICONS.put( CPP_ICON_NAME, new ImageView( CPP_ICON_NAME ));
-		ICONS.put( CS_ICON_NAME,  new ImageView( CS_ICON_NAME ));
-		ICONS.put( LIB_ICON_NAME, new ImageView( LIB_ICON_NAME ));
-	}
-
-
-	private static ImageView getIcon( String iconName ) {
-		return ICONS.get( iconName );
+		ICONS.put( SLN_ICON_NAME, SLN_IMAGE );
+		ICONS.put( CPP_ICON_NAME, CPP_IMAGE );
+		ICONS.put( CS_ICON_NAME,  CS_IMAGE  );
+		ICONS.put( LIB_ICON_NAME, LIB_IMAGE );
 	}
 
 	private Stage _stage;
+	private StatusBar _statusBar;
+
+	private static ImageView getIcon( String iconName ) {
+		return new ImageView( ICONS.get( iconName ));
+	}
+
+	//==============================================================================================
+
+	private static TreeItem< String > createSolutionItem( SolutionFile solutionFile ) {
+		TreeItem< String > solutionItem = new TreeItem<>( solutionFile.path().getFileName().toString() );
+		solutionItem.setExpanded( true );
+		solutionItem.setGraphic( getIcon( SLN_ICON_NAME ));
+
+		return solutionItem;
+	}
+
 
 	//==============================================================================================
 
@@ -50,29 +67,26 @@ public class Main extends Application {
 		MenuBar menuBar = new MenuBar();
 		menuBar.getMenus().add( createFileMenu() );
 
+		_statusBar = new StatusBar();
+
+		SolutionFile solutionFile = new SolutionFile( Paths.get( Settings.solutionFileNames().get( 0 )));
+		TreeItem< String > solutionItem = createSolutionItem( solutionFile );
 
 		BorderPane borderPane = new BorderPane();
 		borderPane.setTop( menuBar );
-		borderPane.setCenter( createDependencyTree( Settings.solutionFileNames().get( 0 )));
+		borderPane.setCenter( new TreeView<>( solutionItem ));
+		borderPane.setBottom( _statusBar );
 
 		Scene scene = new Scene( borderPane );
 		stage.setScene( scene );
 		stage.show();
+
+		buildSolutionTree( solutionFile, solutionItem );
 	}
+
 
 	//==============================================================================================
 
-	private static TreeView< String > createDependencyTree( String solutionFileName ) throws Exception {
-		SolutionFile solutionFile = new SolutionFile( Paths.get( solutionFileName ));
-		TreeItem< String > solutionItem = createSolutionItem( solutionFile );
-
-		for ( ProjectFile projectFile : solutionFile.findProjects() ) {
-			buildProjectTree( solutionItem, projectFile );
-		}
-		return new TreeView<>( solutionItem );
-	}
-
-	//==============================================================================================
 
 	private Menu createFileMenu() {
 		MenuItem open = new MenuItem( "_Open..." );
@@ -103,27 +117,36 @@ public class Main extends Application {
 
 	//==============================================================================================
 
-	private static TreeItem< String > createSolutionItem( SolutionFile solutionFile ) {
-		TreeItem< String > solutionItem = new TreeItem<>( solutionFile.path().getFileName().toString() );
-		solutionItem.setExpanded( true );
-		solutionItem.setGraphic( getIcon( SLN_ICON_NAME ));
-
-		return solutionItem;
+	private void buildSolutionTree( SolutionFile solutionFile, TreeItem< String > solutionItem ) throws Exception {
+		List< ProjectFile > projectFiles = solutionFile.loadProjects();
+		_statusBar.setText( String.format( "Loaded %d projects", projectFiles.size() ));
+		Thread builder = new Thread(() -> {
+			try {
+				for ( ProjectFile projectFile : projectFiles ) {
+					buildProjectTree( solutionFile, solutionItem, projectFile );
+				}
+			} catch ( Exception exception ) {
+				exception.printStackTrace();
+			}
+		});
+		builder.start();
 	}
 
 	//==============================================================================================
 
-	private static void buildProjectTree( TreeItem< String > parentItem , ProjectFile projectFile  ) throws Exception {
+	private static void buildProjectTree( SolutionFile solutionFile, TreeItem< String > parentItem ,
+		ProjectFile projectFile  ) throws Exception {
+
 		TreeItem< String > projectItem = new TreeItem<>( projectFile.path().getFileName().toString() );
 		projectItem.setGraphic( getProjectIcon( projectFile ));
-
-		for ( ProjectFile projectReference : projectFile.getProjectReferences() ) {
+		for ( Path projectReferencePath : projectFile.getProjectReferences() ) {
 			for ( String libraryReference : projectFile.getLibraryReferences() ) {
 				TreeItem< String > libraryItem = new TreeItem<>( libraryReference );
 				libraryItem.setGraphic( getIcon( LIB_ICON_NAME ));
 				projectItem.getChildren().add( libraryItem );
 			}
-			buildProjectTree( projectItem, projectReference );
+			ProjectFile projectReference = solutionFile.findProject( projectReferencePath );
+			buildProjectTree( solutionFile, projectItem, projectReference );
 		}
 		parentItem.getChildren().add( projectItem );
 	}
